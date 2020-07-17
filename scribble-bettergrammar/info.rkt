b8222482ff9910a1d5df03e498ad4cdcaab193ae
William J. Bowman <wjb@williamjbowman.com>
2020-07-17 00:16:56 -0700

    added meta package for installing both lib and doc

---
+++ scribble-bettergrammar/info.rkt
@@ -0,0 +1,6 @@
+#lang info
+(define collection 'multi)
+(define deps '("scribble-bettergrammar-lib" "scribble-bettergrammar-doc"))
+(define implies '("scribble-bettergrammar-lib" "scribble-bettergrammar-doc"))
+(define pkg-desc "A Scribble library for better grammar typesetting.")
+(define pkg-authors '(wilbowma))
